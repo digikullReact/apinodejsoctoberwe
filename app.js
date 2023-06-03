26f95322d97de49afe12fe1d91cb5d073126999d
shubh <shubhamdixit863@gmail.com>
2023-06-03 19:28:10 +0530

    Redirect file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cors=require("cors");
 const {errorMiddleware}=require("./middlewares/middleware");
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(express.static("uploads"));
 app.use("/auth",authRouter)
 app.use("/user",userRouter)
--- routes/fileuploadRoutes.js
+++ routes/fileuploadRoutes.js
@@ -21,7 +21,52 @@ const  multipartMiddleware = multipart({ uploadDir: `${path.join(__dirname,"../u
 
 
 router.post("/",multipartMiddleware,fileUpload)
-router.post("/s3",upload,S3fileUpload)
+router.post("/s3",upload,S3fileUpload);
+
+router.get("/fileRedirect/:fileName",(req,res)=>{
+    const fileName=req.params.fileName;
+    // You have send some kind of html prompt or something
+
+    // this will prompt the user for the pass word --->
+    res.send(`
+
+    <html>
+      <form method="post" action="/uploads/fileDownload/${fileName}"  >
+
+      <input name="password" type="text"/>
+      
+      <button type="submit">Download File</button>
+
+
+      </form>
+
+    </html>
+    
+    
+    `)
+
+
+    //res.redirect(`/uploads/fileDownload/${fileName}`)
+  
+
+})
+
+// this will be renderring the file
+
+router.post("/fileDownload/:fileName",(req,res)=>{
+    console.log(req.body);
+
+    if(req.body.password=="hello"){
+        const fileName=req.params.fileName;
+        res.sendFile( `${path.join(__dirname,"../uploads",fileName)}`)
+        return 
+    }
+  
+    res.send("Unauthorized");
+
+    return 
+})
+
 
 
 
